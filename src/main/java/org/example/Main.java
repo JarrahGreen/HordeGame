33de89893c4dccdbe364f461e109e594b686b665
JarrahGreen <135961440+JarrahGreen@users.noreply.github.com>
2024-02-03 18:55:18 +0000

    general error fixing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ import javax.swing.JFrame;
 public class  Main {
     public static void main(String[] args) {
         // todo Player two needs to shoot
+        // todo Add death screen
+        // todo Add enemies increasing in number
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
